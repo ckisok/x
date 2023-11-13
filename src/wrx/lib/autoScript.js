e5f172eca757ae378c0de761ed70aaac246eb153
champkeh <champion.ge@gmail.com>
2023-11-13 22:14:01 +0800

    增加自动翻页

--- src/wrx/lib/autoScript.js
+++ src/wrx/lib/autoScript.js
@@ -7,21 +7,35 @@
         return new Promise(resolve => setTimeout(resolve, duration))
     }
     let isStop = false
+    let retry = 0
     async function run(cls, duration, scroll = false) {
-        let btn
-        while(btn = document.querySelector('.' + cls)) {
-            if (isStop) {
-                break
+        let btn = document.querySelector('.' + cls)
+        if (!btn) {
+            if (retry <= 6) {
+                retry++
+                console.debug('按钮没有出现，5秒后重试')
+                setTimeout(() => {
+                    run(cls, duration, scroll)
+                }, 5000)
+            } else {
+                console.warn('未找到按钮，超时结束')
             }
-            if (scroll) {
-                window.scroll({top: 100000, behavior: 'smooth'})
-                await sleep(1500)
-                window.scroll({top: 0, behavior: 'smooth'})
-                await sleep(1500)
+        } else {
+            // 重置重试次数
+            retry = 0
+            if (!isStop) {
+                if (scroll) {
+                    window.scroll({top: 100000, behavior: 'smooth'})
+                    await sleep(1500)
+                    window.scroll({top: 0, behavior: 'smooth'})
+                    await sleep(1500)
+                }
+                console.debug('开始翻页')
+                btn.click()
+                setTimeout(() => {
+                    run(cls, duration, scroll)
+                }, randomInteger(duration * 1000, (duration + 1) * 1000 + 500))
             }
-            console.debug('开始翻页')
-            btn.click()
-            await sleep(randomInteger(duration * 1000, (duration + 1) * 1000 + 500))
         }
     }
     chrome.runtime.onMessage.addListener(async (msg) => {
